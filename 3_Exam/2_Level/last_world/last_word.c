6b08028d867f29b5093f1a13d0731c149acb7be1
Warpath Shela <wshela@ox-q3.21-school.ru>
2019-06-23 22:56:57 +0300

    upd

--- 3_Exam/2_Level/last_world/last_word.c
+++ 3_Exam/2_Level/last_world/last_word.c
@@ -6,7 +6,7 @@
 /*   By: wshela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 21:16:34 by wshela            #+#    #+#             */
-/*   Updated: 2019/06/23 21:20:22 by wshela           ###   ########.fr       */
+/*   Updated: 2019/06/23 22:56:27 by wshela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,34 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_last_word
+int		ft_last_word(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	i--;
+	while ((str[i] == ' ' || str[i] == '	') && i >= 0)
+		i--;
+	if (i == -1)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	while (str[i] != ' ' && str[i] != '	' && i != 0)
+		i--;
+	if (str[i] == ' ' || str[i] == '	')
+		i++;
+	while (str[i] != '\0' && str[i] != ' ' && str[i] != '	')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+	return (0);
+}
+
 int		main(int ac, char **av)
 {
 	if (ac == 2)
