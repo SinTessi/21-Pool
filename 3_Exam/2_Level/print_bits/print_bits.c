81c27e0341ae144f759ace699ad83d7bf01b5e09
Warpath Shela <wshela@ox-q3.21-school.ru>
2019-06-25 01:12:37 +0300

    update

--- 3_Exam/2_Level/print_bits/print_bits.c
+++ 3_Exam/2_Level/print_bits/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: wshela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 10:58:52 by wshela            #+#    #+#             */
-/*   Updated: 2019/06/25 00:12:58 by wshela           ###   ########.fr       */
+/*   Updated: 2019/06/25 01:11:12 by wshela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 
 void	print_bits(unsigned char octet)
 {
-	int		str[8];
+	char	str[8];
 	int		i;
 	char	res;
 
 	i = 0;
 	while (i < 8)
 	{
-		str[i] = octet % 2;
+		str[i] = octet % 2 + '0';
 		octet = octet / 2;
 		i++;
 	}
 	i--;
 	while (i >= 0)
 	{
-		res = str[i] + '0';
-		write(1, &res, 1);
+		write(1, &str[i], 1);
 		i--;
 	}
 }
--- 3_Exam/2_Level/reverse_bits/reverse_bits.c
+++ 3_Exam/2_Level/reverse_bits/reverse_bits.c
@@ -6,7 +6,7 @@
 /*   By: wshela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 10:58:52 by wshela            #+#    #+#             */
-/*   Updated: 2019/06/25 00:22:49 by wshela           ###   ########.fr       */
+/*   Updated: 2019/06/25 01:12:10 by wshela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,20 @@
 
 unsigned char	reverse_bits(unsigned char octet)
 {
-	int		str[8];
+	char	str[8];
 	int		i;
-	char	res;
 
 	i = 0;
 	while (i < 8)
 	{
-		str[i] = octet % 2;
+		str[i] = octet % 2 + '0';
 		octet = octet / 2;
 		i++;
 	}
 	i = 0;
 	while (i < 8)
 	{
-		res = str[i] + '0';
-		write(1, &res, 1);
+		write(1, &str[i], 1);
 		i++;
 	}
 	return (0);
