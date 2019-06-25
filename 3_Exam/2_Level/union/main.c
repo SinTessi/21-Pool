eb79cd1b93dbcff0d8e8d9765288a99139c40e2b
Warpath Shela <wshela@ox-q3.21-school.ru>
2019-06-25 14:44:06 +0300

    update

--- 3_Exam/2_Level/union/main.c
+++ 3_Exam/2_Level/union/main.c
@@ -1,52 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   union.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wshela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/25 01:14:11 by wshela            #+#    #+#             */
-/*   Updated: 2019/06/25 14:44:00 by wshela           ###   ########.fr       */
+/*   Created: 2019/06/25 14:40:41 by wshela            #+#    #+#             */
+/*   Updated: 2019/06/25 14:43:15 by wshela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void	ft_putchar(char c);
 
-void	ft_putchar(char c)
+void	ft_union(char **str);
+
+int		main(int ac, char **av)
 {
-	write(1, &c, 1);
-}
-
-void	ft_test(char *str, int *ai)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (ai[str[i]] == 0)
-		{
-			ft_putchar(str[i]);
-			ai[str[i]] = 1;
-			i++;
-		}
-		else
-			i++;
-	}
-}
-
-void	ft_union(char **str)
-{
-	int	ai[128];
-	int i;
-
-	i = 0;
-	while (i < 128)
-	{
-		ai[i] = 0;
-		i++;
-	}
-	ft_test(str[1], ai);
-	ft_test(str[2], ai);
-	ft_putchar('\n');
+	if (ac == 3)
+		ft_union(av);
+	else
+		ft_putchar('\n');
+	return (0);
 }
